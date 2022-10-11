ff173b05ab9327eee3317b3bf8e76f71ee90f2a0
Harun HATİBOĞLU <111196660+Harhat18@users.noreply.github.com>
2022-10-11 23:33:00 +0300

    add

--- src/componenets/Contacts/Form/index.js
+++ src/componenets/Contacts/Form/index.js
@@ -1,18 +1,40 @@
-import React from "react";
+import { useState } from "react";
 
-function Form() {
+function Form({ addContact, contacts }) {
+  console.log(addContact);
+  const [form, setForm] = useState({ fullname: "", phone_number: "" });
+  const onChangeInput = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    if (form.fullname === "" || form.phone_number === "") {
+      return false;
+    }
+    addContact([...contacts, form]);
+  };
   return (
-    <div>
+    <form onSubmit={onSubmit}>
       <div>
-        <input name="fullname" placeholder="Full Name" />
+        <input
+          name="fullname"
+          placeholder="Full Name"
+          onChange={onChangeInput}
+        />
       </div>
       <div>
-        <input name="phone_number" placeholder="Phone Number" />
+        <input
+          name="phone_number"
+          placeholder="Phone Number"
+          onChange={onChangeInput}
+        />
       </div>
       <div>
         <button>Add</button>
       </div>
-    </div>
+    </form>
   );
 }
 
