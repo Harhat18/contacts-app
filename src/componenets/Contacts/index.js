90216c7451e9080e1433ce76e123d6dcc381d1b0
Harun HATİBOĞLU <111196660+Harhat18@users.noreply.github.com>
2022-10-11 23:01:34 +0300

    add

--- src/componenets/Contacts/index.js
+++ src/componenets/Contacts/index.js
@@ -1,7 +1,15 @@
 import React from "react";
 
+import List from "./list";
+import Form from "./Form";
+
 function Contacts() {
-  return <div>Contacts</div>;
+  return (
+    <div>
+      <List />
+      <Form />
+    </div>
+  );
 }
 
 export default Contacts;
